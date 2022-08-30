0b1d0971480f6e9963e56955f5d0c9021ea4ec6e
Gabriel Amado <amadocmgabrieil@gmail.com>
2022-08-30 11:43:10 -0300

    Correção de gamma

--- cor.h
+++ cor.h
@@ -3,19 +3,20 @@
 
 #include "vec3.h"
 #include <iostream>
+#include <cmath>
 
 // Usando Multi-sampling 
 void escrever_cor (std::ostream &saida, Cor cor_pixel, int samples_por_pixel)
 {      
     auto r = cor_pixel.x();
-    auto g = cor_pixel.y();
     auto b = cor_pixel.z();
+    auto g = cor_pixel.y();
 
-    // Dividir a cor pelo número de samples:
     auto scale = 1.0 / samples_por_pixel;
-    r *= scale;
-    g *= scale;
-    b *= scale;
+    // Dividir a cor pelo número de samples e fazer correção de gamma para gamma = 2.0:
+    r = sqrt(scale*r);
+    g = sqrt(scale*g);
+    b = sqrt(scale*b);
  
     // Escrever o valor traduzido [0, 255] de cada componente da cor
     saida << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
